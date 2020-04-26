52ae53efa55b7284989d30e69cb4dcd3d3211c37
jacobklewis <zerogamerfx@gmail.com>
2020-04-26 16:39:58 -0500

    refactored rendering code

--- src/main/java/me/jacoblewis/kotlinjvmdraw/Main.kt
+++ src/main/java/me/jacoblewis/kotlinjvmdraw/Main.kt
@@ -9,37 +9,16 @@ import java.io.File
 import java.io.FileOutputStream
 import kotlin.math.*
 
+val TEST_FRAME: Int? = 50
+val SAVE_AS_MP4 = true
+
+val ANIMATION_LENGTH_SECONDS = 4
+val FPS = 60
+val TOTAL_FRAMES = ANIMATION_LENGTH_SECONDS * FPS
+
+val ANIMATED_VALUES = 0 to 50
+
 fun main() {
-    val TEST_FRAME: Int? = 50
-    val SAVE_AS_MP4 = true
-
-    val ANIMATION_LENGTH_SECONDS = 4
-    val FPS = 60
-    val TOTAL_FRAMES = ANIMATION_LENGTH_SECONDS * FPS
-
-    val ANIMATED_VALUES = 0 to 50
-
-    fun renderFrame(frame: Int): BufferedImage {
-        val frameProgress = expoInterpolator(frame / TOTAL_FRAMES.toFloat(), 2)
-        val currentAnimatedValue = frameProgress * ANIMATED_VALUES.second + (1 - frameProgress) * ANIMATED_VALUES.first
-
-        return DrawEngine.draw(854, 480, Color.WHITE) {
-
-            val radius = height / 3.5
-            val steps = 0..50
-            for (i in steps) {
-                val angle = 2 * PI / steps.last * i
-                val radiusMod = (abs(cos(angle)).pow(currentAnimatedValue.toDouble()) + sin(angle)) * radius
-                val x = cos(angle) * radiusMod + centerX
-                val y = -sin(angle) * radiusMod + centerY + height / 8
-
-                oval(x, y, 10, 10)
-            }
-
-            text("Frame: $frame", centerX, height - height / 10, height / 12)
-        }
-    }
-
     if (TEST_FRAME != null) {
         val imgBytes = renderFrame(TEST_FRAME)
         println("Saving frame: $TEST_FRAME")
@@ -59,6 +38,40 @@ fun main() {
         encoder.finish()
     }
 }
+
+
+fun renderFrame(frame: Int): BufferedImage {
+    val frameProgress = expoInterpolator(frame / TOTAL_FRAMES.toFloat(), 2)
+    val currentAnimatedValue = frameProgress * ANIMATED_VALUES.second + (1 - frameProgress) * ANIMATED_VALUES.first
+    return renderFrame(frame, currentAnimatedValue)
+}
+
+
+
+fun renderFrame(frame: Int, currentAnimatedValue: Float): BufferedImage {
+    return DrawEngine.draw(854, 480, Color.WHITE) {
+        val radius = height / 3.5
+        val steps = 0..50
+        for (i in steps) {
+            val angle = 2 * PI / steps.last * i
+            val radiusMod = (abs(cos(angle)).pow(currentAnimatedValue.toDouble()) + sin(angle)) * radius
+            val x = cos(angle) * radiusMod + centerX
+            val y = -sin(angle) * radiusMod + centerY + height / 8
+
+            oval(x, y, 10, 10)
+        }
+
+        text("Frame: $frame", centerX, height - height / 10, height / 12)
+    }
+}
+
+
+
+
+
+
+
+
 
 private fun saveImage(image: BufferedImage, folderName: String = "imgs", fileName: String = "img") {
     val folder = File(folderName)
